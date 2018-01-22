afaa8b83b3f6136bb23e4439766315d05e96d455
Pawel Smolinski <ps@businessnow.dk>
2018-01-22 09:43:08 +0100

    Modified native files for push notifications (not tested)

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@
 
 import React, { Component } from "react";
 import { Platform, StyleSheet, Text, View, FlatList } from "react-native";
+import OneSignal from "react-native-onesignal";
 
 const instructions = Platform.select({
   ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
@@ -76,6 +77,44 @@ export default class App extends Component {
       ]
     };
   }
+
+  componentDidMount() {
+    OneSignal.configure({});
+    Platform.OS === "ios" ? OneSignal.registerForPushNotifications() : null;
+  }
+
+  componentWillMount() {
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('registered', this.onRegistered);
+    OneSignal.addEventListener('ids', this.onIds);
+}
+
+componentWillUnmount() {
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('registered', this.onRegistered);
+    OneSignal.removeEventListener('ids', this.onIds);
+}
+
+onReceived(notification) {
+    console.log("Notification received: ", notification);
+}
+
+onOpened(openResult) {
+  console.log('Message: ', openResult.notification.payload.body);
+  console.log('Data: ', openResult.notification.payload.additionalData);
+  console.log('isActive: ', openResult.notification.isAppInFocus);
+  console.log('openResult: ', openResult);
+}
+
+onRegistered(notifData) {
+    console.log("Device had been registered for push notifications!", notifData);
+}
+
+onIds(device) {
+console.log('Device info: ', device);
+}
 
   render() {
     let notifications = null;
