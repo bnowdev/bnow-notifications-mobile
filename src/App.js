25fa9de3f92c3cb601a34f0919699b327091be1f
Pawel Smolinski <ps@businessnow.dk>
2018-01-22 11:20:47 +0100

    Update android folder and create seperate index folders

--- src/App.js
+++ src/App.js
@@ -1,101 +1,48 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
+import OneSignal from 'react-native-onesignal';
+import Toast from 'react-native-root-toast';
 
-import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View, FlatList } from "react-native";
+class App extends Component {
+  componentDidMount() {
+    OneSignal.configure({
+      onNotificationOpened: this.handleNotification,
+    });
+  }
 
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
-});
-
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      notifications: [
-        {
-          date: new Date(),
-          title: "Test Notification 1",
-          text: "This is a test a test notifications 1",
-          key: 1
-        },
-        {
-          date: new Date(),
-          title: "Test Notification 2",
-          text: "This is a test notifications 2",
-          key: 2
-        },
-        {
-          date: new Date(),
-          title: "Test Notification 3",
-          text: "This is a test a test notifications 3",
-          key: 3
-        },
-        {
-          date: new Date(),
-          title: "Test Notification 1",
-          text: "This is a test a test notifications 1",
-          key: 4
-        },
-        {
-          date: new Date(),
-          title: "Test Notification 2",
-          text: "This is a test notifications 2",
-          key: 5
-        },
-        {
-          date: new Date(),
-          title: "Test Notification 3",
-          text: "This is a test a test notifications 3",
-          key: 6
-        },
-        {
-          date: new Date(),
-          title: "Test Notification 1",
-          text: "This is a test a test notifications 1",
-          key: 7
-        },
-        {
-          date: new Date(),
-          title: "Test Notification 2",
-          text: "This is a test notifications 2",
-          key: 8
-        },
-        {
-          date: new Date(),
-          title: "Test Notification 3",
-          text: "This is a test a test notifications 3",
-          key: 9
-        }
-      ]
-    };
+  handleNotification(message, data, isActive) {
+    if (isActive) {
+      Toast.show(message);
+    } else {
+      // NOTE: This is the point at which you would tap into your routing system
+      Alert.alert(message, JSON.stringify({ name: data.room }));
+    }
   }
 
   render() {
-    let notifications = null;
-
     return (
       <View style={styles.container}>
-        <View style={styles.navBar}>
-          <Text style={styles.navBarText}>BusinessNow</Text>
-        </View>
-        <Text style={styles.welcome}>List of your notifications</Text>
-
-        <FlatList style={styles.notificationList}
-          data={this.state.notifications}
-          renderItem={({ item }) => (
-            <View  style={styles.notificationContainer}>
-              <Text style={styles.notificationTitle}> {item.title}</Text>
-              <Text style={styles.notificationText}> {item.text}</Text>
-            </View>
-          )}
-        />
+        <Text style={styles.welcome}>
+          Welcome to the OneSignal Example!
+        </Text>
+        <Text style={styles.instructions}>
+          Using {Platform.OS}? Cool.
+        </Text>
+        {Platform.OS === 'ios' ?
+          <TouchableOpacity
+            onPress={() => OneSignal.registerForPushNotifications()}
+            style={{ padding: 20, backgroundColor: '#3B5998' }}
+          >
+            <Text style={{ color: '#fff' }}>Request Push Notification Permission</Text>
+          </TouchableOpacity>
+        : null}
       </View>
     );
   }
@@ -104,39 +51,20 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "flex-start",
-    alignItems: "flex-start",
-    backgroundColor: "#F5FCFF"
-  },
-  navBar: {
-    backgroundColor: "#303a46",
-    width: "100%",
-    height: 50
-  },
-  navBarText: {
-    color: "white",
-    fontSize: 35
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
   welcome: {
     fontSize: 20,
-    // textAlign: "center",
-    margin: 10
+    textAlign: 'center',
+    margin: 10,
   },
-  notificationList: {
-    width: "100%"
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
   },
-  notificationContainer: {
-    flex: 1,
-    flexDirection: "column",
-    marginBottom: 20
-   },
-  notificationTitle: {
-    fontSize: 20,
-    color: "black",
-    flex: 1
-  },
-  notificationText: {
-    color: "grey",
-    fontSize: 10
-  }
 });
+
+export default App;
---
+++ src/App2.js
@@ -0,0 +1,163 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View, FlatList } from "react-native";
+
+const instructions = Platform.select({
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
+  android:
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
+});
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notifications: [
+        {
+          date: new Date(),
+          title: "Test Notification 1",
+          text: "This is a test a test notifications 1",
+          key: 1
+        },
+        {
+          date: new Date(),
+          title: "Test Notification 2",
+          text: "This is a test notifications 2",
+          key: 2
+        },
+        {
+          date: new Date(),
+          title: "Test Notification 3",
+          text: "This is a test a test notifications 3",
+          key: 3
+        },
+        {
+          date: new Date(),
+          title: "Test Notification 1",
+          text: "This is a test a test notifications 1",
+          key: 4
+        },
+        {
+          date: new Date(),
+          title: "Test Notification 2",
+          text: "This is a test notifications 2",
+          key: 5
+        },
+        {
+          date: new Date(),
+          title: "Test Notification 3",
+          text: "This is a test a test notifications 3",
+          key: 6
+        },
+        {
+          date: new Date(),
+          title: "Test Notification 1",
+          text: "This is a test a test notifications 1",
+          key: 7
+        },
+        {
+          date: new Date(),
+          title: "Test Notification 2",
+          text: "This is a test notifications 2",
+          key: 8
+        },
+        {
+          date: new Date(),
+          title: "Test Notification 3",
+          text: "This is a test a test notifications 9",
+          key: 9
+        }
+      ]
+    };
+  }
+
+  // marginLeft: "14%"
+
+  renderSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "100%",
+          backgroundColor: "#CED0CE",
+        }}
+      />
+    );
+  };
+
+  render() {
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.navBar}>
+          <Text style={styles.navBarText}>BusinessNow</Text>
+        </View>
+        <Text style={styles.welcome}>List of your notifications</Text>
+
+        <FlatList
+          style={styles.notificationList}
+          ItemSeparatorComponent={this.renderSeparator}
+          data={this.state.notifications}
+          renderItem={({ item }) => (
+            <View style={styles.notificationContainer}>
+              <Text style={styles.notificationTitle}> {item.title}</Text>
+              <Text style={styles.notificationText}> {item.text}</Text>
+            </View>
+          )}
+        />
+      </View>
+    );
+  }
+}
+
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    backgroundColor: "#F5FCFF"
+  },
+  navBar: {
+    backgroundColor: "#303a46",
+    width: "100%",
+    height: 50
+  },
+  navBarText: {
+    color: "white",
+    fontSize: 35
+  },
+  welcome: {
+    fontSize: 20,
+    margin: 10
+  },
+  notificationList: {
+    width: "100%"
+  },
+  notificationContainer: {
+    flex: 1,
+    flexDirection: "column",
+    marginTop: 5,
+    marginBottom: 5
+  },
+  notificationTitle: {
+    fontSize: 20,
+    color: "black",
+    flex: 1
+  },
+  notificationText: {
+    color: "grey",
+    fontSize: 15
+  },
+  separator: {
+    color: "black"
+  }
+});
